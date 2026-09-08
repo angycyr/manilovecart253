95987ba002036b678ee45ebfaeae3ab170bed2b3
angycyr <angieangela.tao@gmail.com>
2026-09-08 17:21:06 -0400

    idk tried to make second script folder

---
+++ topics/version-control/angyangytest/js/script.js
@@ -0,0 +1,15 @@
+function setup (){
+    createCanvas(900,900);
+    ellipseMode(CENTER)
+    
+    //wowie_cart = loadImage('assets/images/cart_2.png');
+}
+
+function draw() {
+    background('#c48da7');
+    // image(wowie_cart, mouseX, mouseY);
+    
+    rect(49, 22, 25.20)
+    circle(30,30,30)
+    fill('#eb3489')
+}
