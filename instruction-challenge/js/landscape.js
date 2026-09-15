1e1ad922ee4c1407a4cb884cab72f83ccc5d5e4f
angycyr <angieangela.tao@gmail.com>
2026-09-15 15:29:25 -0400

    added background and mountains
    for loop for background gradient
    triangles and quad for mountains

--- instruction-challenge/js/landscape.js
+++ instruction-challenge/js/landscape.js
@@ -1,6 +1,6 @@
 /**
  * Title: birds, where do they come from?
- * By Angy and Sydney
+ * By Angy, Sydney, and ?konstantinos
  * 
  * Description
  * imagine yourself in a landscape. 
@@ -23,5 +23,18 @@ function setup() {
  * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
 */
 function draw() {
-    background(170,120,210)
+    background(110,120,210)
+
+    for (let size = 800; size > 10; size -= 40){
+        circle(width/2, height/2-75,size)
+        fill(210,120,90,20)
+        noStroke();
+    }
+push()
+fill(90,50,140)
+    triangle(0, 600, 300, 400, 500, 500)
+    triangle(450, 450, 750, 300, 950, 500)
+    triangle(-300, 700, 90, 200, 300, 500)
+    quad(700, 450, 450, 900, 900, 900, 900, 475);
+pop()
 }
