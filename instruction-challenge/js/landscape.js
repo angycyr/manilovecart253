9267109568efdf0ba1cb73ac53b73ef71cc8b520
angycyr <angieangela.tao@gmail.com>
2026-09-15 14:56:40 -0400

    set background colour

---
+++ instruction-challenge/js/landscape.js
@@ -0,0 +1,27 @@
+/**
+ * Title: birds, where do they come from?
+ * By Angy and Sydney
+ * 
+ * Description
+ * imagine yourself in a landscape. 
+ * you see rolling hills and a setting sun. 
+ * suddendly, you hear a CAW CAW CAW CAW CAW CAW. 
+ * where did that come from? 
+ * who knows
+
+"use strict";
+
+/**
+ * OH LOOK I DIDN'T DESCRIBE SETUP!!
+*/
+function setup() {
+    createCanvas(900, 900);
+}
+
+
+/**
+ * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+*/
+function draw() {
+    background(170,120,210)
+}
